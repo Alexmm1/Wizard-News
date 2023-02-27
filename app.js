2150f77561e3720917e188b8ef67cc3ada012d78
Alexander <alexmayeux64@gmail.com>
2023-02-27 14:21:41 -0600

    Deploying to Web Server

--- app.js
+++ app.js
@@ -46,61 +46,69 @@ app.get("/", (req, res) => {
   res.send(html);
 });
 
-app.get("/posts/:id", (req, res) => {
-  const id = req.params.id;
-  const post = postBank.find(id);
+// app.get("/posts/:id", (req, res) => {
+//   const id = req.params.id;
+//   const post = postBank.find(id);
 
-  if (!post.id) {
-    res.status(404);
+//   if (!post.id) {
+//     res.status(404);
 
-    const html = `
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <title>Wizard News</title>
-      <link rel="stylesheet" href="/style.css" />
-    </head>
-    <body>
-      <header><img src="/logo.png"/>Wizard News</header>
-      <div class="not-found">
-        <p>404: Page Not Found</p>
-      </div>
-    </body>
-    </html>`;
-    res.send(html);
-  } else {
-    const html = `
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <title>Wizard News</title>
-      <link rel="stylesheet" href="/style.css" />
-    </head>
-    <body>
-      <div class="news-list">
-        <header><img src="/logo.png"/>Wizard News</header>
-        ${`
-          <div class='news-item'>
-            <p>
-              ${post.title}
-              <small>(by ${post.name})</small>
+//     const html = `
+//     <!DOCTYPE html>
+//     <html>
+//     <head>
+//       <title>Wizard News</title>
+//       <link rel="stylesheet" href="/style.css" />
+//     </head>
+//     <body>
+//       <header><img src="/logo.png"/>Wizard News</header>
+//       <div class="not-found">
+//         <p>404: Page Not Found</p>
+//       </div>
+//     </body>
+//     </html>`;
+//     res.send(html);
+//   } else {
+//     const html = `
+//     <!DOCTYPE html>
+//     <html>
+//     <head>
+//       <title>Wizard News</title>
+//       <link rel="stylesheet" href="/style.css" />
+//     </head>
+//     <body>
+//       <div class="news-list">
+//         <header><img src="/logo.png"/>Wizard News</header>
+//         ${`
+//           <div class='news-item'>
+//             <p>
+//               ${post.title}
+//               <small>(by ${post.name})</small>
               
-            </p>
-            <p>
-            ${post.content}
-            </p>
-            <small class="news-info">
-              ${post.date}
-            </small>
-          </div>`}
-      </div>
-    </body>
-  </html>`;
-    res.send(html);
-  }
-});
+//             </p>
+//             <p>
+//             ${post.content}
+//             </p>
+//             <small class="news-info">
+//               ${post.date}
+//             </small>
+//           </div>`}
+//       </div>
+//     </body>
+//   </html>`;
+//     res.send(html);
+//   }
+// });
 
-const PORT = 1337;
+app.get('/post/:id', (req, res) => {
+  const id = req.params.id
+  const post = find(id)
+  if(!post.id) {
+    throw new Error('Not Found')
+  }
+})
+
+const {Port = 1337} = process.env
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
